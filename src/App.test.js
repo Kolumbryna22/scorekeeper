a04b375524fc9f7f488676b7de8a120289f6939e
Dominik Wilusz <wilusz1@o2.pl>
2019-01-06 21:58:18 +0100

    working on app

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import PlayersList from './components/PlayersList/PlayersList';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+    shallow(<App />);
+});
+
+it('should update player score', () => {
+    const onScoreUpdate;
+    const playersAfterUpdate;
+    const appComponent = shallow(<App />);
+    const players = [
+        {
+            name: 'Kunegunda',
+            score: 5,
+        }
+    ];
+
+    appComponent.setState({ players });
+    onScoreUpdate = appComponent.find(PlayersList).prop('onScoreUpdate')
+
+    onScoreUpdate(0, 6);
+
+    playersAfterUpdate = appComponent.state().players;
+
+    expect(playersAfterUpdate).toEqual(6);
 });
